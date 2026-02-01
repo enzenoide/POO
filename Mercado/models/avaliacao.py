ac353b7d943a3a217b14f94709af700fe4f1efe6
Henze Fernandes <henzefernandes1974@gmail.com>
2026-02-01 20:45:51 +0000

    Implementação da classe Avaliação

--- Mercado/models/avaliacao.py
+++ Mercado/models/avaliacao.py
@@ -1,76 +1,82 @@
 from datetime import datetime
 import json
 from models.dao import DAO
+
+
 class Avaliacao:
-    def __init__(self,id,idcliente,idproduto,data,texto):
+    def __init__(self, id, idvenda, idcliente, texto, data=None):
         self.set_id(id)
+        self.set_idvenda(idvenda)
         self.set_idcliente(idcliente)
-        self.set_idproduto(idproduto)
-        self.set_data(data)
         self.set_texto(texto)
-    def get_id(self):
-        return self.__id
-    def get_idcliente(self):
-        return self.__idcliente
-    def get_idproduto(self):
-        return self.__idproduto
-    def get_data(self):
-        return self.__data
-    def get_texto(self):
-        return self.__texto
-    
-    def set_id(self,id):
-        if id < 0:
-            raise ValueError("Não existe ID negativo")
-        else:
-            self.__id = id
-    def set_idcliente(self,idcliente):
-        if idcliente < 0:
-            raise ValueError("Não existe ID negativo")
-        else:
-            self.__idcliente = idcliente
-    def set_idproduto(self,idproduto):
-        if idproduto < 0:
-            raise ValueError("Não existe ID negativo")
-        else:
-            self.__idproduto = idproduto
+        self.set_data(data if data else datetime.now())
+
+    def get_id(self): return self.__id
+    def get_idvenda(self): return self.__idvenda
+    def get_idcliente(self): return self.__idcliente
+    def get_texto(self): return self.__texto
+    def get_data(self): return self.__data
+
+    def set_id(self, id):
+        self.__id = int(id) if id else 0
+
+    def set_idvenda(self, idvenda):
+        if not idvenda:
+            raise ValueError("Venda inválida")
+        self.__idvenda = idvenda
+
+    def set_idcliente(self, idcliente):
+        self.__idcliente = idcliente
+
+    def set_texto(self, texto):
+        if not texto.strip():
+            raise ValueError("Avaliação não pode estar vazia")
+        self.__texto = texto
+
     def set_data(self, data):
-        if isinstance(data, str):
-            data = datetime.fromisoformat(data)
-        if data > datetime.now(): 
-            raise ValueError("Data inválida meu mano")
-    def set_texto(self,texto):
-        if texto == "":
-            raise ValueError("Texto não pode estar vazio")
-        else:
-            self.__texto = texto
+        self.__data = data
+
     def to_json(self):
-        return {"id": self.get_id(),"idcliente": self.get_idcliente(),"idproduto": self.get_idproduto(),"data": self.get_data(),"texto": self.get_texto()}
+        return {
+            "id": self.get_id(),
+            "idvenda": self.get_idvenda(),
+            "idcliente": self.get_idcliente(),
+            "texto": self.get_texto(),
+            "data": self.get_data().isoformat()
+        }
+
     @staticmethod
     def from_json(dic):
-        return Avaliacao(dic["id"],dic["idcliente"],dic["idproduto"],dic["data"],dic["texto"])
+        return Avaliacao(
+            dic["id"],
+            dic["idvenda"],
+            dic["idcliente"],
+            dic["texto"],
+            datetime.fromisoformat(dic["data"])
+        )
+
 
 class AvaliacaoDAO(DAO):
+
     @classmethod
     def abrir(cls):
         cls.objetos = []
         try:
-            with open("json/avaliacao.json", "r") as arquivo:
-                list_dic = json.load(arquivo)
-                for dic in list_dic:
-                    c = Avaliacao.from_json(dic)
-                    cls.objetos.append(c)
-          
-           
-        except Exception as e:
+            with open("json/avaliacoes.json", "r") as arq:
+                for dic in json.load(arq):
+                    cls.objetos.append(Avaliacao.from_json(dic))
+        except:
             pass
+
     @classmethod
     def salvar(cls):
-        with open("json/avaliacao.json", "w") as arquivo:
-            json.dump([{
-                "id": p.get_id(),
-                "icliente": p.get_idcliente(),
-                "idproduto": p.get_idproduto(),
-                "data": p.get_data(),
-                "texto": p.get_texto()
-            } for p in cls.objetos], arquivo, indent=4)
+        with open("json/avaliacoes.json", "w") as arq:
+            json.dump([a.to_json() for a in cls.objetos], arq, indent=4)
+
+    @classmethod
+    def buscar_por_venda(cls, idvenda):
+        cls.abrir()
+        for a in cls.objetos:
+            if a.get_idvenda() == idvenda:
+                return a
+        return None
